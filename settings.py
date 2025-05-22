b787f2ee26b1eb6b9a4d9002487d0b75ce40bfbe
Saniok <banlichelendsh@yandex.ru>
2025-05-22 16:13:57 +0300

    start deploy

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ def settings(app):
 
     # Основные настройки
     app.config['SECRET_KEY'] = os.environ.get(
-        'SECRET_KEY', 'your_default_secret_key'
+        'SECRET_KEY', 'secret_key'
     )
     app.config['DEBUG'] = os.environ.get('DEBUG', 'False').lower() == 'true'
 
